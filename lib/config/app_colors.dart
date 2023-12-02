36aca1701ee247ca5f7c66731d45757105af9a9c
DedeSnrwn13 <sunarwandede13@gmail.com>
2023-12-02 11:25:03 +0700

    feat: register screen

--- lib/config/app_colors.dart
+++ lib/config/app_colors.dart
@@ -6,4 +6,8 @@ class AppColor {
   static const Color sentence = Color(0xFF808080);
   static const Color light = Color(0xFFFFFFFF);
   static const Color dark = Color(0xFF000000);
+  static const Color placeholder = Color(0xFF989595);
+  static const Color backgrounInput = Color(0xFFF2F2F2);
+  static const Color inputLabel = Color(0xFF0F0F0F);
+  static const Color sentenceSecondary = Color(0xFF838383);
 }
--- lib/pages/auth/register_screen.dart
+++ lib/pages/auth/register_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
-import 'package:d_input/d_input.dart';
 import 'package:d_view/d_view.dart';
+import 'package:pemira_app/config/app_assets.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:pemira_app/config/app_colors.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
@@ -20,74 +24,120 @@ class _RegisterScreenState extends State<RegisterScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Padding(
-        padding: const EdgeInsets.all(16.0),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 16,
+          vertical: 24,
+        ),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            DView.height(80),
+            DView.height(14),
             Image.asset(
-              'assets/logo.png', // Ganti dengan path/logo sesuai proyek Anda
-              height: 120,
-              width: 120,
+              AppAssets.logoName,
             ),
             DView.height(16),
-            const Text(
-              'Selamat Datang!',
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
+            Text(
+              'Create an account',
+              style: GoogleFonts.playfairDisplay(
+                fontSize: 40,
+                color: AppColor.heading,
+                fontWeight: FontWeight.w700,
+                fontStyle: FontStyle.normal,
               ),
             ),
-            DView.height(16),
-            DInput(
-              label: 'Nama',
-              hint: 'Masukkan Nama Anda',
-              controller: editName,
-              // prefixIcon: Icons.person,
+            DView.height(12),
+            Text(
+              'To create an account with STTB in Vote, kindly fill out the form below.',
+              style: GoogleFonts.openSans(
+                fontSize: 16,
+                color: AppColor.sentence,
+                fontWeight: FontWeight.w400,
+                fontStyle: FontStyle.normal,
+              ),
             ),
-            DView.height(8),
-            DInput(
-              label: 'Email',
-              hint: 'Masukkan Email Anda',
-              controller: editEmail,
-              // prefixIcon: Icons.email,
-            ),
-            DView.height(8),
-            DInput(
-              label: 'No. Telpon',
-              hint: 'Masukkan No. Telpon Anda',
-              controller: editPhone,
-              // prefixIcon: Icons.phone,
-            ),
-            DView.height(8),
-            DInputPassword(
-              label: 'Password',
-              hint: 'Masukkan Password Anda',
-              controller: editPassword,
-              // prefixIcon: Icons.lock,
+            DView.height(32),
+            _buildTextFieldWithLabelAndIcon(
+              'Name',
+              'Input your name here',
+              Icons.person,
             ),
             DView.height(16),
+            _buildTextFieldWithLabelAndIcon(
+              'Email',
+              'Input your email here',
+              Icons.email,
+            ),
+            DView.height(16),
+            _buildTextFieldWithLabelAndIcon(
+              'Phone Number',
+              'Input your phone here',
+              Icons.phone,
+            ),
+            DView.height(16),
+            _buildTextFieldWithLabelAndIcon(
+              'Password',
+              'Input your password here',
+              Icons.lock,
+            ),
+            DView.height(32),
             ElevatedButton(
               onPressed: () {
                 // Tambahkan logika untuk membuat akun
               },
-              child: const Text('Create Account'),
+              style: ButtonStyle(
+                backgroundColor: const MaterialStatePropertyAll(
+                  AppColor.primary,
+                ),
+                minimumSize: MaterialStateProperty.all(
+                  const Size(
+                    double.infinity,
+                    46,
+                  ),
+                ),
+                shape: MaterialStateProperty.all(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(100),
+                  ),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 100,
+                  vertical: 24,
+                ),
+                child: Text(
+                  'Create Account',
+                  style: GoogleFonts.openSans(
+                    color: AppColor.light,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w700,
+                    fontStyle: FontStyle.normal,
+                  ),
+                ),
+              ),
             ),
-            DView.height(8),
+            DView.height(16),
             Row(
-              mainAxisAlignment: MainAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.start,
               children: [
-                const Text('Sudah punya akun?'),
-                DView.height(4),
+                Text(
+                  'Have an account already click? ',
+                  style: GoogleFonts.openSans(
+                    color: AppColor.sentenceSecondary,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w400,
+                  ),
+                ),
                 InkWell(
                   onTap: () {
                     // Tambahkan navigasi ke halaman login
                   },
-                  child: const Text(
+                  child: Text(
                     'LOGIN',
-                    style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.bold,
+                    style: GoogleFonts.openSans(
+                      fontSize: 16,
+                      color: AppColor.primary,
+                      fontWeight: FontWeight.w700,
                     ),
                   ),
                 ),
@@ -96,6 +146,58 @@ class _RegisterScreenState extends State<RegisterScreen> {
           ],
         ),
       ),
+    );
+  }
+
+  Widget _buildTextFieldWithLabelAndIcon(
+    String label,
+    String placeholder,
+    IconData icon, {
+    bool isPassword = false,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: GoogleFonts.openSans(
+            fontSize: 16,
+            fontWeight: FontWeight.w400,
+            fontStyle: FontStyle.normal,
+            color: AppColor.inputLabel,
+          ),
+        ),
+        DView.height(6),
+        Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(100),
+            color: AppColor.backgrounInput,
+          ),
+          child: Padding(
+            padding: const EdgeInsets.all(12),
+            child: TextField(
+              obscureText: isPassword,
+              decoration: InputDecoration(
+                filled: true,
+                fillColor: AppColor.backgrounInput,
+                hoverColor: Colors.transparent,
+                hintText: placeholder,
+                suffixIcon: Icon(
+                  icon,
+                  size: 24,
+                ),
+                border: InputBorder.none,
+                hintStyle: GoogleFonts.openSans(
+                  color: AppColor.placeholder,
+                  fontSize: 16,
+                  fontWeight: FontWeight.w400,
+                  fontStyle: FontStyle.normal,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
